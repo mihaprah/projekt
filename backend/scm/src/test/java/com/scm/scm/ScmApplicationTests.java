c27c5bc318ae10abcbdb80fce77d815b2d92716b
Matija Krsnik <111431985+Matija334@users.noreply.github.com>
2024-04-30 09:08:52 +0200

    Springboot init

---
+++ backend/scm/src/test/java/com/scm/scm/ScmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.scm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
