836bbef1fa83939c8be385edadd461faf6b8496d
mihaprah <miha.prah1@gmail.com>
2024-05-15 23:35:30 +0200

    Fix Navbar.tsx added styles

--- frontend/scm/src/Components/Navbar/Navbar.tsx
+++ frontend/scm/src/Components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@
 import styles from './Navbar.module.css';
 import Link from "next/link";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUser} from "@fortawesome/free-regular-svg-icons";
+import { faMagnifyingGlass, faUsersRectangle, faCircleUser} from "@fortawesome/free-solid-svg-icons";
 import {useEffect, useState} from "react";
 import {usePathname} from 'next/navigation';
 import {onAuthStateChanged, signOut} from "firebase/auth";
@@ -41,11 +41,17 @@ const Navbar = () => {
                 <div className={"items-center justify-center flex"}>
                     <nav className={"navbar"}>
                         <ul>
-                            <li className={"m-2 p-3 rounded-8 font-semibold hover:bg-primary-dark dark:hover:bg-primary-light"}>
-                                <Link href={"/tenants"}>Tenants</Link>
+                            <li className={"m-2 p-3 rounded-8 hover:bg-primary-dark hover:scale-105 transition hover:shadow dark:hover:bg-primary-light "}>
+                                <Link href={"/tenants"}>
+                                    <span className={"mr-2 font-semibold"}>Tenants</span>
+                                    <FontAwesomeIcon className={"h-5 w-auto ml-1"} icon={faUsersRectangle}/>
+                                </Link>
                             </li>
-                            <li className={"m-2 p-3 rounded-8 font-semibold hover:bg-primary-dark dark:hover:bg-primary-light"}>
-                                <Link href={"/saved-searches"}>Saved Searches</Link>
+                            <li className={"m-2 p-3 rounded-8 hover:bg-primary-dark hover:scale-105 transition hover:shadow dark:hover:bg-primary-light"}>
+                            <Link href={"/saved-searches"}>
+                                    <span className={"mr-2 font-semibold"}>Saved Searches</span>
+                                    <FontAwesomeIcon className={"h-5 w-auto ml-1"} icon={faMagnifyingGlass} />
+                                </Link>
                             </li>
                         </ul>
                     </nav>
@@ -56,13 +62,15 @@ const Navbar = () => {
                 {user && (
                     <div className="dropdown dropdown-end">
                         <div tabIndex={0} role="button"
-                             className="btn bg-primary-light dark:bg-primary-dark text-white border-none m-1 shadow-none hover:bg-primary-dark dark:hover:bg-primary-light">
+                             className="btn bg-primary-light dark:bg-primary-dark text-white border-none m-1 shadow-none hover:bg-primary-dark hover:scale-105 transition hover:shadow dark:hover:bg-primary-light">
                             <span className={"p-2 font-medium"}>{user.email}</span>
-                            <FontAwesomeIcon className={`${styles.icon} p-2`} icon={faUser}/>
+                            <FontAwesomeIcon className={`${styles.icon} p-2`} icon={faCircleUser} />
                         </div>
                         <ul tabIndex={0}
                             className="dropdown-content z-[1] menu p-2 shadow-none rounded-8 w-52 bg-primary-light dark:bg-primary-dark">
-                            <li className={"p-3 hover:bg-primary-dark dark:hover:bg-primary-light hover:rounded-8"} onClick={handleLogout}>Logout</li>
+                            <li className={"px-3 py-2 mx-2 rounded-8 hover:bg-primary-dark dark:hover:bg-primary-light hover:scale-105 transition hover:cursor-pointer"} onClick={handleLogout}>
+                                <span>Logout</span>
+                            </li>
                         </ul>
                     </div>
                 )}
