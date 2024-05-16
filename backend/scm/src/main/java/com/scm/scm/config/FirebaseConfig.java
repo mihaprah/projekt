fb86e77abc119ea7ef4d7a2c2c73f384c5c62a09
Matija Krsnik <111431985+Matija334@users.noreply.github.com>
2024-05-16 14:56:44 +0200

    Firebase config

--- backend/scm/src/main/java/com/scm/scm/config/FirebaseConfig.java
+++ backend/scm/src/main/java/com/scm/scm/config/FirebaseConfig.java
@@ -3,23 +3,28 @@ package com.scm.scm.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 @Configuration
 public class FirebaseConfig {
 
     @Bean
     public FirebaseApp firebaseApp() throws IOException {
-        InputStream serviceAccount = getClass().getClassLoader().getResourceAsStream("serviceAccountKey.json");
-
-        if (serviceAccount == null) {
-            throw new RuntimeException("Service account key file not found");
+        String firebaseConfig = System.getenv("FIREBASE_SERVICE_ACCOUNT_KEY");
+        if (firebaseConfig == null) {
+            throw new RuntimeException("Firebase service account key not found in environment variables");
         }
+
+        Map<String, Object> firebaseCredentials = new Gson().fromJson(firebaseConfig, Map.class);
+        InputStream serviceAccount = new ByteArrayInputStream(new Gson().toJson(firebaseCredentials).getBytes(StandardCharsets.UTF_8));
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
