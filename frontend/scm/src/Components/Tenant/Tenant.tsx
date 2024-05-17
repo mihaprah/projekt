caff3514fac7246e82202b84c9f46d83bc804950
anzo26 <anze.gol10@gmail.com>
2024-05-17 15:45:52 +0200

    Added component Tenant, page tenants

---
+++ frontend/scm/src/Components/Tenant/Tenant.tsx
@@ -0,0 +1,24 @@
+import {Tenant as TenantModel} from '../../models/Tenant';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUsers} from "@fortawesome/free-solid-svg-icons";
+
+interface Props {
+    tenant: TenantModel;
+}
+
+const Tenant: React.FC<Props> = ({tenant}) => (
+
+        <div className="card mx-11 shadow-xl m-7 rounded-8">
+            <div className="card-body">
+                <h2 className="card-title">{tenant.title}</h2>
+                <p>{tenant.description}</p>
+                <p><FontAwesomeIcon className={"h-5 w-auto ml-1 mt-8 mr-2"} icon={faUsers}/>{tenant.users.length}</p>
+            </div>
+            <figure>
+            <div style={{backgroundColor: tenant.colorCode, height: '50px', width: '100%'}}></div>
+            </figure>
+        </div>
+
+);
+
+export default Tenant;
---
+++ frontend/scm/src/app/tenants/page.tsx
@@ -0,0 +1,69 @@
+import {cookies}  from "next/headers";
+import Tenant from "../../Components/Tenant/Tenant";
+import {Tenant as TenantModel} from "../../models/Tenant";
+import {faPlus} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+const fetchTenants = async (IdToken: string): Promise<TenantModel[]> => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tenants/user`, {
+            headers: {
+                'userToken': `Bearer ${IdToken}`,
+            },
+        });
+
+        if (!res.ok) {
+            throw new Error(`Error fetching tenants: ${res.statusText}`);
+        }
+
+        const tenants = await res.json();
+
+        if (!Array.isArray(tenants)) {
+            throw new Error('Fetched data is not an array');
+        }
+
+        return tenants;
+    } catch (error) {
+        console.error('Failed to fetch tenants:', error);
+        return [];
+    }
+}
+
+
+const TenantsPage: React.FC = async () => {
+    const IdToken = cookies().get('IdToken')?.value || '';
+
+    if (!IdToken) {
+        throw new Error('IdToken is not available');
+    }
+
+    const tenants = await fetchTenants(IdToken);
+
+    return (
+        <div>
+            <div className="flex justify-between items-center">
+                <h1 className="text-3xl mx-10 pt-5 text-secondary-dark font-semibold">Tenants</h1>
+                <button
+                    className="btn mt-4 mx-10 px-5 btn-sm bg-primary-light border-0 text-white dark:bg-primary-dark dark:hover:bg-primary-dark rounded-8 font-semibold hover:scale-105 transition hover:bg-primary-dark">
+                    Add new <FontAwesomeIcon className={"text-secondary-light font-semibold"} icon={faPlus}/>
+                </button>
+            </div>
+            <div className="flex space-x-2">
+                {tenants.length === 0 ? (
+                    <p className="text-center text-2xl mx-auto mt-10">No tenants available!</p>
+                ) : (
+                    tenants.length === 1 ? (
+                        <p>Only 1 tenant need to redirect but not working at the moment!!!! </p>
+                        /*window.location.href = `/contacts/${tenants[0].tenantUniqueName}`*/
+                    ) : (
+                        tenants.map((tenant) => (
+                            <Tenant key={tenant.id} tenant={tenant}/>
+                        ))
+                    )
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default TenantsPage;
