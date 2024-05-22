75a0e4c1513be639b80ab4ddccb2ea063a7822f3
Matija Krsnik <111431985+Matija334@users.noreply.github.com>
2024-05-22 11:30:52 +0200

    Updated tests for Export

--- backend/scm/src/test/java/com/scm/scm/contact/ContactExportTest.java
+++ backend/scm/src/test/java/com/scm/scm/contact/ContactExportTest.java
@@ -1,50 +1,43 @@
 package com.scm.scm.contact;
 
 import com.google.firebase.auth.FirebaseToken;
-import com.scm.scm.config.FirebaseConfig;
-import com.scm.scm.contact.rest.ContactController;
+import com.scm.scm.contact.services.ContactServices;
 import com.scm.scm.support.export.ExportContactExcel;
 import com.scm.scm.support.export.ExportContactRequest;
 import com.scm.scm.support.security.UserAccessService;
 import com.scm.scm.support.security.UserVerifyService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import java.util.Collections;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
-@SpringBootTest
-class ContactExportTest {
-
-    @Autowired
-    private ContactController contactController;
+class ExportContactExcelTest {
 
     @Mock
-    private ExportContactExcel exportContactExcel;
-
-    @MockBean
-    private UserAccessService userAccessService;
+    private ContactServices contactServices;
 
     @Mock
     private UserVerifyService userVerifyService;
 
-    @MockBean
-    private FirebaseConfig firebaseConfig;
+    @Mock
+    private UserAccessService userAccessService;
 
+    @InjectMocks
+    private ExportContactExcel exportContactExcel;
 
     @BeforeEach
-    public void init() throws Exception{
-        try (AutoCloseable ac = MockitoAnnotations.openMocks(this)){}
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
     @Test
@@ -54,12 +47,13 @@ class ContactExportTest {
 
         when(mockToken.getEmail()).thenReturn("test@example.com");
         when(userVerifyService.verifyUserToken(anyString())).thenReturn(mockToken);
-        when(userAccessService.hasAccessToContact(mockToken.getEmail(), null)).thenReturn(true);
-        when(userAccessService.hasAccessToTenant(mockToken.getEmail(), null)).thenReturn(true);
+        when(userAccessService.hasAccessToTenant(anyString(), anyString())).thenReturn(true);
+        when(userAccessService.hasAccessToContact(anyString(), anyString())).thenReturn(true);
 
         ExportContactRequest request = new ExportContactRequest(null, null, Collections.emptyList());
-        ResponseEntity<byte[]> response = contactController.exportContacts(request, userToken);
-        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        ExportContactExcel exportContactExcel = new ExportContactExcel(contactServices);
+        ResponseEntity<byte[]> response = exportContactExcel.exportContacts("tenantUniqueName", Collections.emptyList());
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
     }
 
     @Test
@@ -69,12 +63,12 @@ class ContactExportTest {
 
         when(mockToken.getEmail()).thenReturn("test@example.com");
         when(userVerifyService.verifyUserToken(anyString())).thenReturn(mockToken);
-        when(userAccessService.hasAccessToTenant(mockToken.getEmail(), "tenantId")).thenReturn(false);
-        when(userAccessService.hasAccessToContact(mockToken.getEmail(), null)).thenReturn(false);
+        when(userAccessService.hasAccessToTenant(anyString(), anyString())).thenReturn(false);
+        when(userAccessService.hasAccessToContact(anyString(), anyString())).thenReturn(false);
 
-        ExportContactRequest request = new ExportContactRequest( "tenantUniqueName", "tenantId", Collections.emptyList());
-        ResponseEntity<byte[]> response = contactController.exportContacts(request, userToken);
-        assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+        ExportContactRequest request = new ExportContactRequest("tenantUniqueName", "tenantId", Collections.emptyList());
+        ExportContactExcel exportContactExcel = new ExportContactExcel(contactServices);
+        ResponseEntity<byte[]> response = exportContactExcel.exportContacts("tenantUniqueName", Collections.emptyList());
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
     }
-
 }
