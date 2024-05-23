a7fb820e75667f994c93d3315ea34464844f6fae
mihaprah <miha.prah1@gmail.com>
2024-05-23 09:09:44 +0200

    Test Deployment fix

--- backend/scm/src/main/java/com/scm/scm/ScmApplication.java
+++ backend/scm/src/main/java/com/scm/scm/ScmApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ScmApplication {
+	public String PORT = System.getenv("PORT");
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScmApplication.class, args);
