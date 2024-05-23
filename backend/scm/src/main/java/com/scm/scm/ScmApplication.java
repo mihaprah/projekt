c32c97b450d05ff3763270bb569c968e12821e8c
mihaprah <miha.prah1@gmail.com>
2024-05-23 10:39:11 +0200

    Test Deployment fix

--- backend/scm/src/main/java/com/scm/scm/ScmApplication.java
+++ backend/scm/src/main/java/com/scm/scm/ScmApplication.java
@@ -6,11 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ScmApplication {
 	public String PORT = System.getenv("PORT");
-	public String DATABASE_URL = System.getenv("DATABASE_URL");
-	public String DATABASE_NAME = System.getenv("DATABASE_NAME");
-	public String DATABASE_USERNAME = System.getenv("DATABASE_USERNAME");
-	public String DATABASE_PASSWORD = System.getenv("DATABASE_PASSWORD");
-	public String DATABASE_PORT = System.getenv("DATABASE_PORT");
+	public String DATABASE_URI = System.getenv("DATABASE_URI");
 
 
 	public static void main(String[] args) {
