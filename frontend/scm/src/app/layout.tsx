e8958b30318aeb1914529c8ef8f2ef199dfa3006
Matija Krsnik <111431985+Matija334@users.noreply.github.com>
2024-05-23 10:54:24 +0200

    Update layout

--- frontend/scm/src/app/layout.tsx
+++ frontend/scm/src/app/layout.tsx
@@ -1,13 +1,15 @@
+// RootLayout.tsx
 "use client";
-import {Inter} from "next/font/google";
+
+import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/Components/Navbar/Navbar";
 import ClientOnly from "@/Components/ClientOnly/ClientOnly";
-import {Suspense} from "react";
+import { Suspense } from "react";
 import Loading from "@/app/loading";
 import AuthCheck from "@/Components/AuthCheck/AuthCheck";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
                                        children,
@@ -18,13 +20,11 @@ export default function RootLayout({
         <html data-theme="winter" lang="en">
         <body className={inter.className}>
         <ClientOnly>
-            <Suspense fallback={<Loading/>}>
-                <Navbar/>
-                <AuthCheck>
-                    <Suspense fallback={<Loading/>}>
+            <Suspense fallback={<Loading />}>
+                <Navbar />
+                    <Suspense fallback={<Loading />}>
                         {children}
                     </Suspense>
-                </AuthCheck>
             </Suspense>
         </ClientOnly>
         </body>
