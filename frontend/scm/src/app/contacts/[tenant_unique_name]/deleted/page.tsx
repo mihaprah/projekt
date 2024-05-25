6fa0f3f3d5667f9b4f1168be005652c9c061bea9
anzo26 <anze.gol10@gmail.com>
2024-05-25 11:33:23 +0200

    Add deleted contacts page, delete contact permanently

---
+++ frontend/scm/src/app/contacts/[tenant_unique_name]/deleted/page.tsx
@@ -0,0 +1,45 @@
+import {cookies} from "next/headers";
+import {redirect} from "next/navigation";
+import ContactsPage from "@/app/contacts/[tenant_unique_name]/page";
+import {Tenant as TenantModel} from "@/models/Tenant";
+import DeletedContacts from "@/Components/Contact/DeletedContacts";
+
+const fetchTenant = async (tenant_unique_name: string, IdToken: string): Promise<TenantModel> => {
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tenants/unique/${tenant_unique_name}`, {
+            headers: {
+                'userToken': `Bearer ${IdToken}`,
+            },
+        });
+
+        if (!res.ok) {
+            throw new Error(`Error fetching tenant: ${res.statusText}`);
+        }
+
+        return await res.json();
+    } catch (error) {
+        console.error('Failed to fetch tenant:', error);
+        return {} as TenantModel;
+    }
+};
+
+const DeletedContactsPage = async (props: { params: {tenant_unique_name: string}}) => {
+    const { params } = props;
+    const { tenant_unique_name } = params;
+    const IdToken = cookies().get('IdToken')?.value || '';
+
+    if (!IdToken) {
+        redirect('/login');
+    }
+
+    const tenant = await fetchTenant(tenant_unique_name, IdToken);
+
+
+    return (
+        <div className="container mx-auto p-4">
+            <DeletedContacts IdToken={IdToken} tenant={tenant} />
+        </div>
+  );
+}
+
+export default DeletedContactsPage;
